70a5a79e2892b29a55deb4d9bbc501f544d4e34f
Richard <richard@richards-imac.local>
2018-09-12 11:53:51 +0100

    Initial Commit

---
+++ How are you/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  How are you
+//
+//  Created by Richard on 05/09/2018.
+//  Copyright © 2018 Richard Pereira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
